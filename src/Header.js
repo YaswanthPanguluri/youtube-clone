eab2044cb46476fb84fef556079bb09069f3f1b9
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-06-23 16:39:03 -0500

    Added Material UI

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+function Header() {
+  return (
+    <div className="header">
+    <h1>I am a header</h1>
+    <MenuIcon />
+    </div>
+  )
+}
+
+export default Header
