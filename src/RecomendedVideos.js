b45b73904079c6c7a3dcfe1fdaa25c705d1d94bf
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-06-24 15:00:04 -0500

    Recomended Completed

--- src/RecomendedVideos.js
+++ src/RecomendedVideos.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import "./RecomendedVideos.css"
+import React from "react";
+import "./RecomendedVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecomendedVideos() {
   return (
-    <div className='RecomendedVideos'><h1>RecomendedVideos</h1></div>
-  )
+    <div className="RecomendedVideos">
+      <h2>Recomended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Yaswanth youtube clone"
+          views="2.3M views"
+          timeStamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/ytc/AGIKgqMgEhHAUOcGBHBhWLImIcN47payeZ1CENgeKkeoMg=s176-c-k-c0x00ffffff-no-rj"
+          channel="Yaswanth Panguluri"
+          image="https://img.freepik.com/free-photo/abstract-fire-desktop-wallpaper-realistic-blazing-flame-image_53876-147448.jpg?w=2000"
+        />
+
+
+        <VideoCard
+          title="Yaswanth youtube clone"
+          views="2.3M views"
+          timeStamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/ytc/AGIKgqMgEhHAUOcGBHBhWLImIcN47payeZ1CENgeKkeoMg=s176-c-k-c0x00ffffff-no-rj"
+          channel="Yaswanth Panguluri"
+          image="https://img.freepik.com/free-photo/abstract-fire-desktop-wallpaper-realistic-blazing-flame-image_53876-147448.jpg?w=2000"
+        />
+
+
+
+        <VideoCard
+          title="Yaswanth youtube clone"
+          views="2.3M views"
+          timeStamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/ytc/AGIKgqMgEhHAUOcGBHBhWLImIcN47payeZ1CENgeKkeoMg=s176-c-k-c0x00ffffff-no-rj"
+          channel="Yaswanth Panguluri"
+          image="https://img.freepik.com/free-photo/abstract-fire-desktop-wallpaper-realistic-blazing-flame-image_53876-147448.jpg?w=2000"
+        />
+
+
+
+        <VideoCard
+          title="Yaswanth youtube clone"
+          views="2.3M views"
+          timeStamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/ytc/AGIKgqMgEhHAUOcGBHBhWLImIcN47payeZ1CENgeKkeoMg=s176-c-k-c0x00ffffff-no-rj"
+          channel="Yaswanth Panguluri"
+          image="https://img.freepik.com/free-photo/abstract-fire-desktop-wallpaper-realistic-blazing-flame-image_53876-147448.jpg?w=2000"
+        />
+
+
+
+        <VideoCard
+          title="Yaswanth youtube clone"
+          views="2.3M views"
+          timeStamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/ytc/AGIKgqMgEhHAUOcGBHBhWLImIcN47payeZ1CENgeKkeoMg=s176-c-k-c0x00ffffff-no-rj"
+          channel="Yaswanth Panguluri"
+          image="https://img.freepik.com/free-photo/abstract-fire-desktop-wallpaper-realistic-blazing-flame-image_53876-147448.jpg?w=2000"
+        />
+
+
+
+        <VideoCard
+          title="Yaswanth youtube clone"
+          views="2.3M views"
+          timeStamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/ytc/AGIKgqMgEhHAUOcGBHBhWLImIcN47payeZ1CENgeKkeoMg=s176-c-k-c0x00ffffff-no-rj"
+          channel="Yaswanth Panguluri"
+          image="https://img.freepik.com/free-photo/abstract-fire-desktop-wallpaper-realistic-blazing-flame-image_53876-147448.jpg?w=2000"
+        />
+
+        
+      </div>
+    </div>
+  );
 }
 
-export default RecomendedVideos
+export default RecomendedVideos;
